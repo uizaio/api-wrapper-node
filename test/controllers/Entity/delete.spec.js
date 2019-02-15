8e26d09e64ca1d3b32930990a57b95a2e37f32c4
Le Khanh Duy <lekhanhduyduy1201@gmail.com>
2019-02-15 16:13:12 +0700

    TEST: update entity function

--- test/controllers/Entity/delete.spec.js
+++ test/controllers/Entity/delete.spec.js
@@ -2,138 +2,139 @@ const expect = require('chai').expect;
 const nock = require('nock');
 
 const workspaceApiDomain = 'https://your-workspace-api-domain.uiza.co';
+const id = 'd1781e62-2d2c-4e3c-b8de-e808e50ac845';
 const path = '/api/public/v3/media/entity';
 const uiza = require('../../../lib/uiza.js')(workspaceApiDomain);
 const errorMessages = require('../../../lib/utils/Errors')();
 
-const POST_DATA_FOR_CREATING_VIDEO = {
-  'name': 'Test Video',
-  'url': 'https://example.com/video.mp4',
-  'inputType': 'http',
-  'description': 'test',
+const DELETE_DATA_RESPONSE = {
+  "data": {
+    "id": "dd934a87-3342-41fa-8cdf-6381b6d92105"
+  },
+  "version": 3,
+  "datetime": "2018-06-15T18:52:45.755Z",
+  'name': '--Title edited--',
 }
 
 describe('Entity-Controller', function () {
-  it('entity: create successfully', async () => {
+  it('/DELETE: delete successfully', async () => {
     nock(workspaceApiDomain)
-      .post(path)
+      .delete(path)
       .reply(200, {
-        ...POST_DATA_FOR_CREATING_VIDEO,
+        ...DELETE_DATA_RESPONSE,
         "code": 200,
       });
 
-    const result = await uiza.entity.create({
-      ...POST_DATA_FOR_CREATING_VIDEO
-    });
+    const result = await uiza.entity.delete({id});
     expect(result.code).eq(200)
   });
 
-  it('entity: create missing parameters', async () => {
+  it('/DELETE: delete missing parameters', async () => {
     nock(workspaceApiDomain)
-      .post(path)
+      .delete(path)
       .reply(400, {
         "code": 400,
       });
 
-    const result = await uiza.entity.create({
+    const result = await uiza.entity.delete({
       'name': 'Test Video',
     });
     expect(result.message).eq(errorMessages.getMessage(result.type))
   });
 
-  it('entity: unauthorized', async () => {
+  it('/DELETE: unauthorized', async () => {
     nock(workspaceApiDomain)
-      .post(path)
+      .delete(path)
       .reply(401, {
-        ...POST_DATA_FOR_CREATING_VIDEO,
+        ...DELETE_DATA_RESPONSE,
         "code": 401,
       });
 
-    const result = await uiza.entity.create({
+    const result = await uiza.entity.delete({
       'name': 'Test Video',
     });
     expect(result.message).eq(errorMessages.getMessage(result.type))
   });
 
-  it('entity: not found', async () => {
+  it('/DELETE: not found', async () => {
     nock(workspaceApiDomain)
-      .post(path)
+      .delete(path)
       .reply(404, {
-        ...POST_DATA_FOR_CREATING_VIDEO,
+        ...DELETE_DATA_RESPONSE,
         "code": 404,
       });
 
-    const result = await uiza.entity.create({
+    const result = await uiza.entity.delete({
       'name': 'Test Video',
     });
     expect(result.message).eq(errorMessages.getMessage(result.type))
   });
 
-  it('entity: wrong parameter', async () => {
+  it('/DELETE: wrong parameter', async () => {
     nock(workspaceApiDomain)
-      .post(path)
+      .delete(path)
       .reply(422, {
-        ...POST_DATA_FOR_CREATING_VIDEO,
+        ...DELETE_DATA_RESPONSE,
         "code": 422,
       });
 
-    const result = await uiza.entity.create({
+    const result = await uiza.entity.delete({
       'name': 'Test Video',
     });
     expect(result.message).eq(errorMessages.getMessage(result.type))
   });
 
-  it('entity: had a problem with our server', async () => {
+  it('/DELETE: had a problem with our server', async () => {
     nock(workspaceApiDomain)
-      .post(path)
+      .delete(path)
       .reply(500, {
-        ...POST_DATA_FOR_CREATING_VIDEO,
+        ...DELETE_DATA_RESPONSE,
         "code": 500,
       });
 
-    const result = await uiza.entity.create({
+    const result = await uiza.entity.delete({
       'name': 'Test Video',
     });
     expect(result.message).eq(errorMessages.getMessage(result.type))
   });
 
-  it('entity: service is unable', async () => {
+  it('/DELETE: service is unable', async () => {
     nock(workspaceApiDomain)
-      .post(path)
+      .delete(path)
       .reply(503, {
-        ...POST_DATA_FOR_CREATING_VIDEO,
+        ...DELETE_DATA_RESPONSE,
         "code": 503,
       });
 
-    const result = await uiza.entity.create({
+    const result = await uiza.entity.delete({
       'name': 'Test Video',
     });
     expect(result.message).eq(errorMessages.getMessage(result.type))
   });
 
-  it('entity: caused by the client', async () => {
+  it('/DELETE: caused by the client', async () => {
     nock(workspaceApiDomain)
-      .post(path)
+      .delete(path)
       .reply(450, {
-        ...POST_DATA_FOR_CREATING_VIDEO,
+        ...DELETE_DATA_RESPONSE,
         "code": 450,
       });
 
-    const result = await uiza.entity.create({
+    const result = await uiza.entity.delete({
       'name': 'Test Video',
     });
     expect(result.message).eq(errorMessages.getMessage(result.type))
   });
 
-  it('entity: server has encountered', async () => {
+  it('/DELETE: server has encountered', async () => {
     nock(workspaceApiDomain)
-      .post(path)
+      .delete(path)
       .reply(501, {
-        ...POST_DATA_FOR_CREATING_VIDEO,
+        ...DELETE_DATA_RESPONSE,
         "code": 501,
       });
 
-    const result = await uiza.entity.create({
+    const result = await uiza.entity.delete({
       'name': 'Test Video',
     });
     expect(result.message).eq(errorMessages.getMessage(result.type))
