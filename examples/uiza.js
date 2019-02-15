06591efbc8692ee6086173281fb61950f0374c4e
ngo.van.tien <ngovantien93@gmail.com>
2019-02-15 17:37:05 +0700

    Fix_Params_And_Send_Description_Link

--- examples/uiza.js
+++ examples/uiza.js
@@ -8,35 +8,31 @@ uiza.entity.create({
   'description': 'tes'
 }).then((res) => console.log(res)).catch((err) => console.log(err));
 
-/** retrieve */
-uiza.entity.retrieve({'id': 'd1781e62-2d2c-4e3c-b8de-e808e50ac845'}).then((res) => console.log(res)).catch((err) => console.log(err));
+// /** retrieve */
+uiza.entity.retrieve('d1781e62-2d2c-4e3c-b8de-e808e50ac845').then((res) => console.log(res)).catch((err) => console.log(err));
 
 
-/** list */
+// /** list */
 uiza.entity.list().then((res) => console.log(res)).catch((err) => console.log(err));
 
-/** update */
+// /** update */
 uiza.entity.update({
   'id': '16de511f-5493-400a-9250-2d3b38b1d0be',
   'name': '--Title edited--',
 }).then((res) => console.log(res)).catch((err) => console.log(err));
 
-/** delete */
-uiza.entity.delete({'id': '5f1c78bd-6900-4f59-a807-9c95aad3ed63'}).then((res) => console.log(res)).catch((err) => console.log(err));
+// /** delete */
+uiza.entity.delete('5f1c78bd-6900-4f59-a807-9c95aad3ed63').then((res) => console.log(res)).catch((err) => console.log(err));
 
-/** search */
-uiza.entity.search({'keyword': 'sample'}).then((res) => console.log(res)).catch((err) => console.log(err));
+// /** search */
+uiza.entity.search('sample').then((res) => console.log(res)).catch((err) => console.log(err));
 
-/** publish */
-uiza.entity.publish({
-  'id': '1a6600c0-6ecd-4ca9-9ee1-3125e7c9d9f8',
-}).then((res) => console.log(res)).catch((err) => console.log(err));
+// /** publish */
+uiza.entity.publish('1a6600c0-6ecd-4ca9-9ee1-3125e7c9d9f8').then((res) => console.log(res)).catch((err) => console.log(err));
 
 
-/** get status publish */
-uiza.entity.get_status_publish({
-  'id': '8c6de86e-f468-4226-b476-4f320bda225a',
-}).then((res) => console.log(res)).catch((err) => console.log(err));
+// /** get status publish */
+uiza.entity.get_status_publish('8c6de86e-f468-4226-b476-4f320bda225a').then((res) => console.log(res)).catch((err) => console.log(err));
 
-/** get aws upload key */
+// /** get aws upload key */
 uiza.entity.get_aws_upload_key().then((res) => console.log(res)).catch((err) => console.log(err));
