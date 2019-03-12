4cf34325c7b595fde042211ffce693bf606361fd
ngo.van.tien <ngovantien93@gmail.com>
2019-03-12 18:04:49 +0700

    Update_Category_V4

--- examples/Entity.js
+++ examples/Entity.js
@@ -1,6 +1,7 @@
-const uiza = require('../lib/uiza')('your-workspace-api-domain.uiza.co', 'your-authorization-key');
-uiza.setAppId('d6342a7b4a6c40d2b851a54a4442ea83')
-// /** get */
+const uiza = require('../lib/uiza')('your-authorization-key');
+uiza.setAppId('your-appId')
+
+/** get */
 uiza.entity.get({
-  id: '797d4a02-cd33-40e3-912f-737cd6b3adda'
+  id: '797d4a02-cd33-40e3-912f-737cd6b3adda',
 }).then((res) => console.log(res)).catch((err) => console.log(err));
